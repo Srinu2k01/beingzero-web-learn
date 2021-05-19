6bf13df961308d7800156aae9e84042e86843d89
Srinu2k01 <srinivasvssj@gmail.com>
2021-05-19 21:56:58 +0530

    crud app 2 commit

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 module.exports.conn=function(){
     var password=process.env.Mongo_atlas_password;
-    var connectionstring="mongodb+srv://system:"+password+"@cluster0.suio8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    var connectionstring="mongodb+srv://system:"+"vssj1944"+"@cluster0.suio8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     mongoose.connect(connectionstring,{useNewUrlParser:true,useUnifiedTopology:true});
     mongoose.connection.on('connected',function(){
     console.log("Database Connected");
